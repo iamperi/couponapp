1a5ce9000c642c9ec5e397e802ed589d13101d3c
pieldefoca <betibaskonia@gmail.com>
2021-03-25 17:36:38 +0100

    Go back

--- resources/views/admin/shops/table-body.blade.php
+++ resources/views/admin/shops/table-body.blade.php
@@ -5,7 +5,7 @@
     <td>{{ $shopUser->email }}</td>
     <td>{{ $shopUser->phone ?? '-' }}</td>
     <td>{{ $shopUser->shop->due_amount ?? 0 }} €</td>
-    @if($shopUser->shop && !is_null($shopUser->shop->registration_token))
+    @if(!is_null($shopUser->shop->registration_token))
     <td>
         <a href="{{ route('admin.shops.send_registration_email', $shopUser->shop) }}" class="btn btn-xs">@lang('Resend email')</a>
     </td>
